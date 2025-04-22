d34b80a1483c6064c01366d9f05c28fd1f659ca4
Maxim Barannikov <mbarannikov@ptsecurity.com>
2025-04-22 03:51:44 +0300

    formatting

--- project/modules/users/service.py
+++ project/modules/users/service.py
@@ -1,6 +1,7 @@
+from fastapi import HTTPException, status
+
 from project.modules.users.models import UserCreate, UserOut
 from project.modules.users.repository import UserRepository
-from fastapi import HTTPException, status
 
 
 class UserService:
@@ -10,8 +11,5 @@ class UserService:
     async def register_user(self, user_data: UserCreate) -> UserOut:
         created_user = await self.repository.create_user(user_data.name, user_data.email, user_data.password)
         if created_user is None:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Failed to create user"
-            )
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Failed to create user")
         return UserOut(**created_user)
