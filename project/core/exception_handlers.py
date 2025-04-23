b56c0b38919ae7e30f7f0df680dfa93ac426e28e
Maxim Barannikov <mbarannikov@ptsecurity.com>
2025-04-23 23:43:58 +0300

    Change example.env and add exception_handlers logs

--- project/core/exception_handlers.py
+++ project/core/exception_handlers.py
@@ -4,8 +4,13 @@ from asyncpg.exceptions import PostgresError, UniqueViolationError  # type: igno
 from fastapi.responses import JSONResponse
 from starlette.requests import Request
 
+from project.config.base import settings
+
+logger = settings.LOGGER.getChild(__name__)
+
 
 async def unique_violation_handler(_: Request, exc: Any) -> JSONResponse:
+    logger.error(f"Unique violation error: {exc}")
     if isinstance(exc, UniqueViolationError):
         detail = str(exc).split("DETAIL:")[-1].strip() if "DETAIL:" in str(exc) else "Unique constraint violated"
         return JSONResponse(status_code=409, content={"error": "Conflict", "detail": detail})
@@ -13,6 +18,7 @@ async def unique_violation_handler(_: Request, exc: Any) -> JSONResponse:
 
 
 async def postgres_error_handler(_: Request, exc: Any) -> JSONResponse:
+    logger.error(f"Postgres error: {exc}")
     if isinstance(exc, PostgresError):
         return JSONResponse(status_code=400, content={"error": "Database error", "detail": str(exc)})
     return JSONResponse(status_code=400, content={"error": "Database error"})
